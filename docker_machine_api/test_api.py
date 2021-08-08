cac70e5c8212560932789b59662b115770ea2946
Arno Duvenhage <aduvenhage@gmail.com>
2021-08-08 22:47:27 +0200

    more testing
    Signed-off-by: Arno Duvenhage <aduvenhage@gmail.com>

--- docker_machine_api/test_api.py
+++ docker_machine_api/test_api.py
@@ -1,40 +1,60 @@
 
 import logging
 import time
+import os
 
 from cl_api import DockerMachine
 
 
-def test_api():
-    logging.basicConfig(level=20)
+def start_render_machine(token, scenario):
     logger = logging.getLogger(__name__)
+    logger.info("TOKEN %s", token)
 
-    # create machine API
+    # create new docker machine
     dm = DockerMachine(name='raytracer',
-                       cwd='../',
+                       cwd='./../raytracer',
                        config={
                             'driver': 'digitalocean', 
                             'digitalocean-image': 'ubuntu-18-04-x64', 
-                            'digitalocean-access-token': '916d25ba891f579dfe4085f7bff379230705dae1a800bd58b3234508d9745ee7',
+                            'digitalocean-access-token': token,
                             'engine-install-url': 'https://releases.rancher.com/install-docker/19.03.9.sh'
+                       },
+                       user_env={
+                           'SCENARIO': scenario,
+                           'OUTPUT': 'raytraced_frame.jpeg',
+                           'VOLUME': '/root/output/'
                        })
 
-    # watch machine output
-    while True:
+    dm.tskRunServices()
+    return dm
+
+
+if __name__ == "__main__":
+    """
+    For testing only ...
+    """
+    logging.basicConfig(level=20)
+    logger = logging.getLogger(__name__)
+
+    dm = start_render_machine(os.environ['TOKEN'], 'scene2')
+
+    # wait for rendering to complete
+    idle = False
+    while dm.busy() or not idle:
+        idle = True
         try:
             text = dm._stdout_queue.get(block=False)
             logger.info(text)
+            idle = False
         except Exception:
             pass
 
         try:
             text = dm._stderr_queue.get(block=False)
             logger.error(text)
+            idle = False
         except Exception:
             pass
 
-        time.sleep(0.1)
-
-
-if __name__ == "__main__":
-    test_api()
+        if idle:
+            time.sleep(0.1)
