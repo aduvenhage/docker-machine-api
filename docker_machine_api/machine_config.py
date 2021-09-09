971d63d739f16ce730c9a108b56cba9cc7751524
Arno Duvenhage <aduvenhage@gmail.com>
2021-09-09 13:06:09 +0200

    added a missing file
    Signed-off-by: Arno Duvenhage <aduvenhage@gmail.com>

---
+++ docker_machine_api/machine_config.py
@@ -0,0 +1,39 @@
+import os
+
+
+class DigitalOceanConfig:
+    def __init__(self, token=None, region=None, type=None, image=None, docker_install=None):
+        self._region = region or 'ams3'
+        self._type = type or 's-1vcpu-2gb-amd'
+        self._token = token or os.getenv('DO_API_TOKEN')
+        self._image = image or 'ubuntu-18-04-x64'
+        self._docker_install = docker_install or 'https://releases.rancher.com/install-docker/19.03.9.sh'
+
+    def config(self):
+        return {
+            'driver': 'digitalocean',
+            'digitalocean-region': self._region,
+            'digitalocean-size': self._type,
+            'digitalocean-image': self._image,
+            'digitalocean-access-token': self._token,
+            'engine-install-url': self._docker_install
+        }
+
+
+class AwsConfig:
+    def __init__(self, access_key=None, secret_key=None, region=None, type=None, image=None):
+        self._region = region or 'eu-central-1'
+        self._type = type or 't2.micro'
+        self._access_key = access_key or os.getenv('AWS_ACCESS_KEY')
+        self._secret_key = secret_key or os.getenv('AWS_SECRET_KEY')
+        self._image = image or 'ami-0b1deee75235aa4bb'
+
+    def config(self):
+        return {
+            'driver': 'amazonec2',
+            'amazonec2-access-key': self._access_key,
+            'amazonec2-secret-key': self._secret_key,
+            'amazonec2-region': self._region,
+            'amazonec2-instance-type': self._type,
+            'amazonec2-ami': self._image
+        }
