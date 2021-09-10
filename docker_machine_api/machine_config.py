8f88abfb693a35a6e271754d4ea0e15eb77cadbc
Arno Duvenhage <aduvenhage@gmail.com>
2021-09-10 12:18:39 +0200

    more config updates
    Signed-off-by: Arno Duvenhage <aduvenhage@gmail.com>

--- docker_machine_api/machine_config.py
+++ docker_machine_api/machine_config.py
@@ -9,6 +9,9 @@ class DigitalOceanConfig:
         self._image = image or 'ubuntu-18-04-x64'
         self._docker_install = docker_install or 'https://releases.rancher.com/install-docker/19.03.9.sh'
 
+    def is_valid(self):
+        return bool(self._token)
+
     def config(self):
         return {
             'driver': 'digitalocean',
@@ -22,11 +25,14 @@ class DigitalOceanConfig:
 
 class AwsConfig:
     def __init__(self, access_key=None, secret_key=None, region=None, type=None, image=None):
-        self._region = region or 'eu-central-1'
+        self._region = region or 'us-east-2'
         self._type = type or 't2.micro'
         self._access_key = access_key or os.getenv('AWS_ACCESS_KEY')
         self._secret_key = secret_key or os.getenv('AWS_SECRET_KEY')
-        self._image = image or 'ami-0b1deee75235aa4bb'
+        self._image = image or 'ami-0b9064170e32bde34'
+
+    def is_valid(self):
+        return bool(self._access_key) and bool(self._secret_key)
 
     def config(self):
         return {
